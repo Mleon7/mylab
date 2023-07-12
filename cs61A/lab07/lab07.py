8026a00d7f508feb791858bfe45f4ef885ced5c9
Mleon7 <2710887576@qq.com>
2023-07-12 17:24:55 +0800

    cs61A 2022 fall lab07 Q1-Q5

---
+++ cs61A/lab07/lab07.py
@@ -0,0 +1,100 @@
+class Account:
+    """An account has a balance and a holder.
+    >>> a = Account('John')
+    >>> a.deposit(10)
+    10
+    >>> a.balance
+    10
+    >>> a.interest
+    0.02
+    >>> a.time_to_retire(10.25) # 10 -> 10.2 -> 10.404
+    2
+    >>> a.balance               # balance should not change
+    10
+    >>> a.time_to_retire(11)    # 10 -> 10.2 -> ... -> 11.040808032
+    5
+    >>> a.time_to_retire(100)
+    117
+    """
+    max_withdrawal = 10
+    interest = 0.02
+
+    def __init__(self, account_holder):
+        self.balance = 0
+        self.holder = account_holder
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        return self.balance
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Insufficient funds"
+        if amount > self.max_withdrawal:
+            return "Can't withdraw that amount"
+        self.balance = self.balance - amount
+        return self.balance
+
+    def time_to_retire(self, amount):
+        """Return the number of years until balance would grow to amount."""
+        assert self.balance > 0 and amount > 0 and self.interest > 0
+        "*** YOUR CODE HERE ***"
+        years = 0
+        temp = self.balance
+        while temp <= amount:
+            temp = temp * (1 + self.interest)
+            years += 1
+        return years
+
+
+class FreeChecking(Account):
+    """A bank account that charges for withdrawals, but the first two are free!
+    >>> ch = FreeChecking('Jack')
+    >>> ch.balance = 20
+    >>> ch.withdraw(100)  # First one's free. Still counts as a free withdrawal even though it was unsuccessful
+    'Insufficient funds'
+    >>> ch.withdraw(3)    # Second withdrawal is also free
+    17
+    >>> ch.balance
+    17
+    >>> ch.withdraw(3)    # Ok, two free withdrawals is enough
+    13
+    >>> ch.withdraw(3)
+    9
+    >>> ch2 = FreeChecking('John')
+    >>> ch2.balance = 10
+    >>> ch2.withdraw(3) # No fee
+    7
+    >>> ch.withdraw(3)  # ch still charges a fee
+    5
+    >>> ch.withdraw(5)  # Not enough to cover fee + withdraw
+    'Insufficient funds'
+    """
+    withdraw_fee = 1
+    free_withdrawals = 2
+
+    "*** YOUR CODE HERE ***"
+    '''
+    def withdraw(self, amount):
+        我自己想的，连题意都没看懂
+        if self.free_withdrawals > 0:
+            self.free_withdrawals -= 1
+            return super().withdraw(amount)
+        else:
+            return super().withdraw(amount + self.withdraw_fee)
+    '''
+    # https://github.com/xuyanshi/cs61a-2022/blob/main/lab/lab07/lab07.py
+    charge = False
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Insufficient funds"
+        if amount > self.max_withdrawal:
+            return "Can't withdraw that amount"
+        if self.charge and amount + self.withdraw_fee > self.balance:
+            return "Insufficient funds"
+        self.balance = self.balance - amount
+        if self.charge:
+            self.balance -= self.withdraw_fee
+        self.charge = True
+        return self.balance
